3d0575f8bf9f08ff1b8f6570010709c6bff4e1e8
AdrsP <144963769+AdrsP@users.noreply.github.com>
2023-11-20 15:04:58 +0000

    traffic light

--- src/js/component/Semaforo.jsx
+++ src/js/component/Semaforo.jsx
@@ -1,44 +1,17 @@
 import React from "react";
 import{ useState, useEffect } from 'react';
 
-const Semaforo = (props) => {
-    const [luzRoja, setluzRoja] = useState("false")
-    const [luzAmarilla, setluzAmarilla] = useState("false")
-    const [luzVerde, setluzVerde] = useState("false")
+const Semaforo = () => {
 
-  
-        if (luzRoja == true) {
-            () => setluzAmarilla("false");
-            () => setluzVerde("false");
-            console.log("luz roja es "+ luzRoja);
-            console.log("luz amarilla es "+ luzAmarilla);
-            console.log("luz verde es " + luzVerde);
-        }
-        if (luzAmarilla == true) {
-            () => setluzRoja("false");
-            () => setluzVerde("false");
-            console.log("luz roja es "+ luzRoja);
-            console.log("luz amarilla es "+ luzAmarilla);
-            console.log("luz verde es " + luzVerde);
-        }
-        if (luzVerde == true) {
-            () => setluzRoja("false");
-            () => setluzAmarilla("false");
-            console.log("luz roja es "+ luzRoja);
-            console.log("luz amarilla es "+ luzAmarilla);
-            console.log("luz verde es " + luzVerde);
-        }
-    
-
-    
+    const [selectedColor, setSelectedColor] = useState("");
 
     return (
         <div>
             <div id="topeDelPoste"></div>
             <div id="cajaDelPoste">
-                <div onClick={() => setluzRoja("true")} className="red"></div>
-                <div onClick={() => setluzAmarilla("true")} className="yellow"></div>
-                <div onClick={() => setluzVerde("true")} className="green"></div>
+                <div onClick={() => setSelectedColor("red")} className={"light red " + ((selectedColor == "red") ? "glowing" : "" )}></div>
+                <div onClick={() => setSelectedColor("yellow")} className={"light yellow " + ((selectedColor == "yellow") ? "glowing" : "" )}></div>
+                <div onClick={() => setSelectedColor("green")} className={"light green " + ((selectedColor == "green") ? "glowing" : "" )}></div>
             </div>
         </div>
         );
